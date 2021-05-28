186d297862daa4d18592aac0e7f7f16441c19e63
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-05-28 15:57:40 +0530

    Update main.py

--- demo1/main.py
+++ demo1/main.py
@@ -32,23 +32,19 @@ def parse_obj(objs):
     for obj in objs:
         if isinstance(obj, pdfminer.layout.LTTextBox):
             for o in obj._objs:
-                if isinstance(o,pdfminer.layout.LTTextLine):
-                    # print(text)
-                    # print(f"X0:{o.x0}, X1{o.x1}, Y0:{o.y0}, Y1:{o.y1}")
-                    # print(f"bbox: {o.bbox}")
-                    # print(f"height: {o.height}, width:{o.width}, word_margin:{o.word_margin}")
-                    text=o.get_text()
+                if isinstance(o, pdfminer.layout.LTTextLine):
+                    text = o.get_text()
                     l.append([o.bbox, o.height, o.width, o.get_text()])
-                    if int(o.x0) not in x0:
-                        x0[int(o.x0)] = {}
-                    if int(o.y0) not in x0[int(o.x0)]:
-                        x0[int(o.x0)][int(o.y0)] = {}
-                    if text not in x0[int(o.x0)][int(o.y0)]:
-                        x0[int(o.x0)][int(o.y0)][text] = {}
-                    x0[int(o.x0)][int(o.y0)][text]["bbox"] = o.bbox
-                    x0[int(o.x0)][int(o.y0)][text]["height"] = o.height
-                    x0[int(o.x0)][int(o.y0)][text]["width"] = o.width
-                    x0[int(o.x0)][int(o.y0)][text]["text"] = o.get_text()
+                    if int(o.y0) not in x0:
+                        x0[int(o.y0)] = {}
+                    if int(o.x0) not in x0[int(o.y0)]:
+                        x0[int(o.y0)][int(o.x0)] = {}
+                    if text not in x0[int(o.y0)][int(o.x0)]:
+                        x0[int(o.y0)][int(o.x0)][text] = {}
+                    x0[int(o.y0)][int(o.x0)][text]["bbox"] = o.bbox
+                    x0[int(o.y0)][int(o.x0)][text]["height"] = o.height
+                    x0[int(o.y0)][int(o.x0)][text]["width"] = o.width
+                    x0[int(o.y0)][int(o.x0)][text]["text"] = o.get_text()
                         # x0[o.x0]["bbox"].append(o.bbox)
                         # x0[o.x0]["height"].append(o.height)
                         # x0[o.x0]["width"].append(o.width)
@@ -80,9 +76,9 @@ l, x0 = parse_obj(layout._objs)
 
 d = {}
 
-for k1 in sorted(x0.keys()):
+for k1 in reversed(sorted(x0.keys())):
     d[k1] = {}
-    for k2 in reversed(sorted(x0[k1].keys())):
+    for k2 in sorted(x0[k1].keys()):
         d[k1][k2] = x0[k1][k2]
 
 import json
